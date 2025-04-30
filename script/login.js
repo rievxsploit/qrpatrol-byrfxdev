971c406526ad80084d805c2e94cbea3fd2def404
rievxsploit <rifaldiwahyu1697@gmail.com>
2025-04-30 08:38:44 +0700

    Update login.js

--- script/login.js
+++ script/login.js
@@ -1,12 +1,14 @@
 function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  const errorBox = document.getElementById("login-error");
 
   auth.signInWithEmailAndPassword(email, password)
     .then(() => {
       window.location.href = "dashboard.html";
     })
     .catch((error) => {
-      alert(error.message);
+      errorBox.textContent = error.message;
+      errorBox.classList.remove("hidden");
     });
 }
