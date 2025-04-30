f5fe12c0f484b12802b2aface38dbe7a5435b371
rievxsploit <rifaldiwahyu1697@gmail.com>
2025-04-30 07:44:31 +0700

    Update firebase-config.js

--- script/firebase-config.js
+++ script/firebase-config.js
@@ -6,6 +6,7 @@ const firebaseConfig = {
   storageBucket: "qrpatrol-8bffa.firebasestorage.app",
   messagingSenderId: "754593461619",
   appId: "1:754593461619:web:5b61abddcdaa310203c106",
+  measurementId: "G-G39HTJZVZC"
 };
 
 firebase.initializeApp(firebaseConfig);
