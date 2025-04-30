2c1a6d364cff188b04d00f0f30c1f16c088c143a
rievxsploit <rifaldiwahyu1697@gmail.com>
2025-04-30 08:41:41 +0700

    Update firebase-config.js

--- script/firebase-config.js
+++ script/firebase-config.js
@@ -2,12 +2,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyArIi3LoUzzKEMzuex4WJr-iIhYxQWCi-k",
   authDomain: "qrpatrol-8bffa.firebaseapp.com",
   projectId: "qrpatrol-8bffa",
-  storageBucket: "qrpatrol-8bffa.appspot.com", // ✅ diperbaiki
+  storageBucket: "qrpatrol-8bffa.appspot.com",
   messagingSenderId: "754593461619",
   appId: "1:754593461619:web:5b61abddcdaa310203c106",
   measurementId: "G-G39HTJZVZC"
 };
 
+// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Inisialisasi Auth dan Firestore
 const auth = firebase.auth();
 const db = firebase.firestore();
